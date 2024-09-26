c3ea84adeb4adce5908e14327a21d394b277b801
“SingireddyLohithReddy” <Nagoor1247@gmail.com>
2024-09-26 19:57:01 +0530

    first commit

---
+++ Test Demo/Test_DemoApp.swift
@@ -0,0 +1,16 @@
+//
+//  Test_DemoApp.swift
+//  Test Demo
+//
+//
+
+import SwiftUI
+
+@main
+struct Test_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
